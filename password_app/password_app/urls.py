22ef87a7728654aca33fd6e30dbf7225ea1e64b4
Marilyn Olejniczak <92530551+ZyanneM@users.noreply.github.com>
2024-01-09 16:03:20 +0100

    Add app url to urls project file

--- password_app/password_app/urls.py
+++ password_app/password_app/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+    path('password_manager/', include('password_manager.urls')),
     path('admin/', admin.site.urls),
 ]
