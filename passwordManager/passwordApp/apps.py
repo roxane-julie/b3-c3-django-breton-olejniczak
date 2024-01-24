8b402f1d0f8865d88e708f196186c3f1f6de0cb5
Roxane Breton <julie.roxane@outlook.fr>
2024-01-24 21:35:22 +0100

    update TD django

--- passwordManager/passwordApp/apps.py
+++ passwordManager/passwordApp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class PasswordManagerConfig(AppConfig):
+class PasswordappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'password_manager'
+    name = 'passwordApp'
