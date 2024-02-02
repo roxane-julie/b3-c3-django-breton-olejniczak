cb0952c5f604fad55ed4c301d00d715f4e1e210e
Marilyn Olejniczak <92530551+ZyanneM@users.noreply.github.com>
2024-02-02 10:41:06 +0100

    Create data model

--- passwordManager/passwordApp/models.py
+++ passwordManager/passwordApp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+import datetime
+from django.db import models
+
+# Create your models here.
+class PassWordManagerDataModel(models.Model):
+    userName = models.CharField(max_length=100)
+    websiteName = models.CharField(max_length=100)
+    websiteUrl = models.URLField(blank=True, default='')
+    password = models.CharField(max_length=100)
