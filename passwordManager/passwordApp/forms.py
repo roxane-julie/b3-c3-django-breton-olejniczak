6a3d355587cbc04bcdb797a5481e526420449b1c
Marilyn Olejniczak <92530551+ZyanneM@users.noreply.github.com>
2024-02-03 19:17:35 +0100

    Redesign signUp form

--- passwordManager/passwordApp/forms.py
+++ passwordManager/passwordApp/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class SignUpForm(forms.Form):
-    username = forms.CharField(max_length=100)
-    email = forms.EmailField()
-    password = forms.CharField(widget=forms.PasswordInput)
+    username = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'placeholder': 'JavascriptLover'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder': 'monemail@test.fr'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'monpassword'}))
