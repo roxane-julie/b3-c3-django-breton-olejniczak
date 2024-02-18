b1533f7794e41cab0d28009415dbfd060fefbb5c
Marilyn Olejniczak <92530551+ZyanneM@users.noreply.github.com>
2024-02-18 23:41:29 +0100

    comment bcrypt which is not use for the moment

--- passwordManager/passwordApp/models.py
+++ passwordManager/passwordApp/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-import bcrypt
+# import bcrypt
 
 # Create your models here.
 class PassWordManagerDataModel(models.Model):
@@ -17,21 +17,21 @@ class PassWordManagerDataModel(models.Model):
     def set_website_name(self, website_name):
         self.websiteName = website_name
 
-    def set_password(self, raw_password):
-        hashed = bcrypt.hashpw(raw_password.encode('utf-8'), bcrypt.gensalt())
-        self.password = hashed.decode('utf-8')
+    # def set_password(self, raw_password):
+    #     hashed = bcrypt.hashpw(raw_password.encode('utf-8'), bcrypt.gensalt())
+    #     self.password = hashed.decode('utf-8')
 
-    def check_password(self, raw_password):
-        return bcrypt.checkpw(raw_password.encode('utf-8'), self.password.encode('utf-8'))
+    # def check_password(self, raw_password):
+    #     return bcrypt.checkpw(raw_password.encode('utf-8'), self.password.encode('utf-8'))
     
     def get_website_data(self):
         return [self.websiteName, self.websiteUrl, self.password]
     
-    def set_website_data(self, website_name, website_url, raw_password):
-        self.websiteName = website_name
-        self.websiteUrl = website_url
-        hashed = bcrypt.hashpw(raw_password.encode('utf-8'), bcrypt.gensalt())
-        self.password = hashed.decode('utf-8')
+    # def set_website_data(self, website_name, website_url, raw_password):
+    #     self.websiteName = website_name
+    #     self.websiteUrl = website_url
+    #     hashed = bcrypt.hashpw(raw_password.encode('utf-8'), bcrypt.gensalt())
+    #     self.password = hashed.decode('utf-8')
 
 class SafeBox(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
